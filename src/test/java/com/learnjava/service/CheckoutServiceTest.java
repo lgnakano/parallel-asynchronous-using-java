d5fbeff71da7d8188f5f368d042c9d8b9e5bbf9f
Luis Nakano <lgnakano@gmail.com>
2022-03-28 16:33:25 -0400

    Added testing for Checkout Service, including tests that would require more cores that the machine has.

---
+++ src/test/java/com/learnjava/service/CheckoutServiceTest.java
@@ -0,0 +1,69 @@
+package com.learnjava.service;
+
+import com.learnjava.domain.checkout.Cart;
+import com.learnjava.domain.checkout.CheckoutResponse;
+import com.learnjava.domain.checkout.CheckoutStatus;
+import com.learnjava.util.DataSet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckoutServiceTest {
+
+    PriceValidatorService priceValidatorService = new PriceValidatorService();
+    CheckoutService checkoutService = new CheckoutService(priceValidatorService);
+
+    @Test
+    void checkout_6_items() {
+
+
+        // ginev
+
+        Cart cart = DataSet.createCart(6);
+
+        // when
+
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+
+        // then
+
+        assertEquals(CheckoutStatus.SUCCESS, checkoutResponse.getCheckoutStatus());
+    }
+
+    @Test
+    void no_of_cores(){
+        System.out.println("no of cores: " + Runtime.getRuntime().availableProcessors());
+    }
+
+    @Test
+    void checkout_49_items() {
+
+        // ginev
+
+        Cart cart = DataSet.createCart(49);
+
+        // when
+
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+
+        // then
+
+        assertEquals(CheckoutStatus.FAILURE, checkoutResponse.getCheckoutStatus());
+    }
+
+    @Test
+    void checkout_25_items() {
+
+        // ginev
+
+        Cart cart = DataSet.createCart(25);
+
+        // when
+
+        CheckoutResponse checkoutResponse = checkoutService.checkout(cart);
+
+        // then
+
+        assertEquals(CheckoutStatus.FAILURE, checkoutResponse.getCheckoutStatus());
+    }
+}
