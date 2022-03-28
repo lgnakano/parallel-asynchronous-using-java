d2de98ca7b14119e421ec284cd1746269fd66823
Luis Nakano <lgnakano@gmail.com>
2022-03-28 15:48:12 -0400

    Added CheckoutService.

---
+++ src/main/java/com/learnjava/service/CheckoutService.java
@@ -0,0 +1,42 @@
+package com.learnjava.service;
+
+import com.learnjava.domain.checkout.Cart;
+import com.learnjava.domain.checkout.CartItem;
+import com.learnjava.domain.checkout.CheckoutResponse;
+import com.learnjava.domain.checkout.CheckoutStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.learnjava.util.CommonUtil.startTimer;
+import static com.learnjava.util.CommonUtil.timeTaken;
+
+public class CheckoutService {
+
+    private final PriceValidatorService priceValidatorService;
+
+    public CheckoutService(PriceValidatorService priceValidatorService) {
+        this.priceValidatorService = priceValidatorService;
+    }
+    public CheckoutResponse checkout(Cart cart) {
+        startTimer();
+
+        List<CartItem> priceValidationList = cart.getCartItemList()
+                .stream()
+                .map(cartItem -> {
+                    boolean isPriceInvalid = priceValidatorService.isCartItemInvalid(cartItem);
+                    cartItem.setExpired(isPriceInvalid);
+                    return cartItem;
+                })
+                .filter(CartItem::isExpired)
+                .collect(Collectors.toList());
+        if (!priceValidationList.isEmpty()) {
+            timeTaken();
+            return new CheckoutResponse(CheckoutStatus.FAILURE, priceValidationList);
+        }
+        else {
+            timeTaken();
+            return new CheckoutResponse(CheckoutStatus.SUCCESS);
+        }
+    }
+}
