3d7e2eca55a3f21f65f8012140a3f7be9807605e
Luis Nakano <lgnakano@gmail.com>
2022-03-28 15:14:47 -0400

    Add ParameterizedTest case for stringTranform1 to compare paraller and sequential processing.

--- src/test/java/com/learnjava/parallelstreams/ParallelStreamsExampleTest.java
+++ src/test/java/com/learnjava/parallelstreams/ParallelStreamsExampleTest.java
@@ -2,11 +2,12 @@ package com.learnjava.parallelstreams;
 
 import com.learnjava.util.DataSet;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static com.learnjava.util.CommonUtil.startTimer;
-import static com.learnjava.util.CommonUtil.timeTaken;
+import static com.learnjava.util.CommonUtil.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParallelStreamsExampleTest {
@@ -22,6 +23,24 @@ class ParallelStreamsExampleTest {
         startTimer();
         List<String> resultList = parallelStreamsExample.stringTransform(inputList);
         timeTaken();
+        stopWatchReset();
+
+        //then
+        assertEquals(inputList.size(), resultList.size());
+        resultList.forEach(name -> assertTrue(name.contains("-")));
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false, true})
+    void stringTransform_1(boolean isParallel) {
+
+        // given
+        List<String> inputList = DataSet.namesList();
+        //when
+        startTimer();
+        List<String> resultList = parallelStreamsExample.stringTransform1(inputList, isParallel);
+        timeTaken();
+        stopWatchReset();
 
         //then
         assertEquals(inputList.size(), resultList.size());
