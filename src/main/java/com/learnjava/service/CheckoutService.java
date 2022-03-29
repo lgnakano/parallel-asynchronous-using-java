3d3fe9acb963b2267589ce97e2ea2555ac1f737c
Luis Nakano <lgnakano@gmail.com>
2022-03-29 18:15:05 -0400

    Add calculateFinalPrice using sum or reduce.

--- src/main/java/com/learnjava/service/CheckoutService.java
+++ src/main/java/com/learnjava/service/CheckoutService.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.learnjava.util.CommonUtil.*;
+import static com.learnjava.util.LoggerUtil.log;
+import static java.util.stream.Collectors.summingDouble;
 
 public class CheckoutService {
 
@@ -30,11 +32,34 @@ public class CheckoutService {
                 .collect(Collectors.toList());
         timeTaken();
         stopWatchReset();
+        
         if (!priceValidationList.isEmpty()) {
             return new CheckoutResponse(CheckoutStatus.FAILURE, priceValidationList);
         }
-        else {
-            return new CheckoutResponse(CheckoutStatus.SUCCESS);
-        }
+   
+        double finalPrice = calculateFinalPrice_reduce(cart);
+        log("Checkout complete and the final price is " + finalPrice);
+        
+        return new CheckoutResponse(CheckoutStatus.SUCCESS, finalPrice);
+    }
+
+    private double calculateFinalPrice(Cart cart) {
+
+        return cart.getCartItemList()
+                .parallelStream()
+//                .map(cartItem -> cartItem.getQuantity() * cartItem.getRate())
+//                .collect(summingDouble(Double::doubleValue));
+                .mapToDouble(cartItem -> cartItem.getQuantity() * cartItem.getRate())
+                .sum();
+    }
+
+    private double calculateFinalPrice_reduce(Cart cart) {
+
+        return cart.getCartItemList()
+                .parallelStream()
+                .map(cartItem -> cartItem.getQuantity() * cartItem.getRate())
+//                .reduce(0.0, (x,y) -> x+y);
+                .reduce(0.0, Double::sum);
+
     }
 }
