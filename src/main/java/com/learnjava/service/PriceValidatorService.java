0af175c2d8333c5ce94d5d260efb2612a8b49f88
Luis Nakano <lgnakano@gmail.com>
2022-03-30 16:41:45 -0400

    Log cart item when checking if it is valid to show ForkJoinPool worker, and simplify validation logic.

--- src/main/java/com/learnjava/service/PriceValidatorService.java
+++ src/main/java/com/learnjava/service/PriceValidatorService.java
@@ -4,15 +4,15 @@ package com.learnjava.service;
 import com.learnjava.domain.checkout.CartItem;
 
 import static com.learnjava.util.CommonUtil.delay;
+import static com.learnjava.util.LoggerUtil.log;
 
 public class PriceValidatorService {
 
     public boolean isCartItemInvalid(CartItem cartItem){
         int cartId = cartItem.getItemId();
+        log("isCartItemInvalid: " + cartItem);
+
         delay(500);
-        if (cartId == 7 || cartId == 9 || cartId == 11) {
-            return true;
-        }
-        return false;
+        return cartId == 7 || cartId == 9 || cartId == 11;
     }
 }
